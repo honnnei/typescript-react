18f7093171784a985e0f0aa8e007fe9b0677a18c
honnnei <hannpop@gmail.com>
2020-10-13 12:23:02 +0200

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import TextField from './TextField';
-
+// import './App.css';
+// import Country from './components/Country';
+// https://opentdb.com/api.php?amount=10
 function App() {
+
+  // const myFunction = (bob: string) => {
+  //   console.log(bob)
+  // }
+
+  // fn={myFunction("hello")}
+
   return (
     <div className="App">
-     <TextField text="hello" />
+    <h1>Quiz</h1>
     </div>
   );
 }
